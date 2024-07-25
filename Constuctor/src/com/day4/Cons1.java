a3220e324cd9ef97637e850d1cbb6303d434aff0
user <user@H-Sys45>
2024-07-25 16:39:46 +0530

    hiiii

---
+++ Constuctor/src/com/day4/Cons1.java
@@ -0,0 +1,17 @@
+package com.day4;
+
+public class Cons1 {
+	Cons1() {
+		System.out.println();
+		}
+	Cons1(int a) {
+		System.out.println("the value of a is"+a);
+}
+	Cons1(float b) {
+		System.out.println("the value of a is"+b);}
+	Cons1(int a,int b) {
+		System.out.println("the value of a & b is"+a +b);
+	}}
+		
+	
+	
---
+++ Constuctor/src/com/day4/Cons2.java
@@ -0,0 +1,15 @@
+package com.day4;
+
+public class Cons2 {
+
+	public static void main(String[] args) {
+		Cons1 C = new Cons1(1,2);
+		Cons1 C1 = new Cons1(1);
+		Cons1 C2 = new Cons1(1f);
+		
+				
+		
+
+	}
+
+}
