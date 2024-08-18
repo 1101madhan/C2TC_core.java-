0d28a781eedd4dcac95ee5afabf891b67fd78980
ELCOT <ELCOT@Lenovo>
2024-08-18 07:50:03 +0530

    poly

---
+++ Exception/src/com/tnsif/daytwelve/InvalidMarksException.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwelve;
+
+public class InvalidMarksException {
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		
+	}
+
+	
+}
